79243d06186f7bbb30c26fce00de9b9f3a6b16da
avi2397 <avisinghr2312@gmail.com>
2020-10-02 23:22:14 +0000

    modified port variable in main.js

--- main.js
+++ main.js
@@ -35,8 +35,8 @@ app.get("/about", function(req, res){
 });
 
 
-
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, process.env.IP, function(){
 	console.log("server started");
 });  
 
